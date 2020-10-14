3db1835285a9a21e9f6de1fef0583aaca8660bc0
spike <bank.tellr@gmail.com>
2020-10-14 22:55:05 +0900

    Adds Float2_2 type

--- src/math/matrices/float2_2.rs
+++ src/math/matrices/float2_2.rs
@@ -1,9 +1,21 @@
-use crate::math::vectors::vector2::Vector2;
-use crate::math::matrices::matrix2_2::Matrix2_2;
-use crate::math::scalars::float::ScalarFloat;
-use crate::math::vectors::float2::Float2;
+use std::ops::{Add, Sub, Mul, Div};
+use crate::math::{
+  scalars::float::ScalarFloat,
+  vectors::{
+    vector2::Vector2,
+    float2::Float2
+  },
+  matrices::{
+    matrix2_2::{
+      Matrix2_2,
+      Matrix2_2Constructor
+    },
+    ops2_2::Matrix2_2Ops
+  }
+};
 
 #[allow(non_camel_case_types)]
+#[derive(Debug, PartialEq)]
 pub struct Float2_2 {
   pub(self) a00: ScalarFloat,
   pub(self) a01: ScalarFloat,
@@ -12,25 +24,70 @@ pub struct Float2_2 {
 }
 
 impl Matrix2_2<Float2, ScalarFloat> for Float2_2 {
+  fn a00(&self) -> ScalarFloat {
+    self.a00
+  }
+
+  fn a01(&self) -> ScalarFloat {
+    self.a01
+  }
+
+  fn a10(&self) -> ScalarFloat {
+    self.a10
+  }
+
+  fn a11(&self) -> ScalarFloat {
+    self.a11
+  }
+}
+
+impl Matrix2_2Constructor<Float2, ScalarFloat> for Float2_2 {
   fn new(a00: ScalarFloat, a01: ScalarFloat,
          a10: ScalarFloat, a11: ScalarFloat) -> Self {
     Self { a00, a01,
            a10, a11 }
   }
+}
 
-  fn a00(&self) -> &ScalarFloat {
-    &self.a00
+impl From<(Float2, Float2)> for Float2_2 {
+  fn from(cols: (Float2, Float2)) -> Self {
+    Self::new(
+      cols.0.x(), cols.1.x(),
+      cols.0.y(), cols.1.y()
+    )
   }
+}
 
-  fn a01(&self) -> &ScalarFloat {
-    &self.a01
+impl Matrix2_2Ops<Float2, ScalarFloat> for Float2_2 {}
+
+impl Add for Float2_2 {
+  type Output = Self;
+
+  fn add(self, rhs: Self) -> Self::Output {
+    <Self as Matrix2_2Ops<Float2, ScalarFloat>>::add(&self, &rhs)
   }
+}
 
-  fn a10(&self) -> &ScalarFloat {
-    &self.a10
+impl Sub for Float2_2 {
+  type Output = Self;
+
+  fn sub(self, rhs: Self) -> Self::Output {
+    <Self as Matrix2_2Ops<Float2, ScalarFloat>>::sub(&self, &rhs)
   }
+}
 
-  fn a11(&self) -> &ScalarFloat {
-    &self.a11
+impl Mul<ScalarFloat> for Float2_2 {
+  type Output = Self;
+
+  fn mul(self, rhs: ScalarFloat) -> Self::Output {
+    <Self as Matrix2_2Ops<Float2, ScalarFloat>>::mul(&self, &rhs)
+  }
+}
+
+impl Div<ScalarFloat> for Float2_2 {
+  type Output = Self;
+
+  fn div(self, rhs: ScalarFloat) -> Self::Output {
+    <Self as Matrix2_2Ops<Float2, ScalarFloat>>::div(&self, &rhs)
   }
 }
--- src/tests/test_matrices.rs
+++ src/tests/test_matrices.rs
@@ -1,9 +1,20 @@
-use crate::math::matrices::int2_2::Int2_2;
-use crate::math::vectors::int2::Int2;
-use crate::math::matrices::ops2_2::Matrix2_2Ops;
-use crate::math::matrices::matrix2_2::Matrix2_2Constructor;
-use crate::math::scalars::int::ScalarInt;
-use crate::math::scalars::scalar::ScalarTrait;
+use crate::math::{
+  matrices::{
+    matrix2_2::Matrix2_2Constructor,
+    ops2_2::Matrix2_2Ops,
+    int2_2::Int2_2,
+    float2_2::Float2_2
+  },
+  vectors::{
+    int2::Int2,
+    float2::Float2
+  },
+  scalars::{
+    scalar::ScalarTrait,
+    int::ScalarInt,
+    float::ScalarFloat
+  }
+};
 
 #[test]
 fn mat2_2_identity() {
@@ -11,8 +22,21 @@ fn mat2_2_identity() {
   let zero = ScalarInt::zero();
   let one = ScalarInt::one();
 
-  assert_eq!(m2id, Int2_2::new(one, zero,
-                               zero, one));
+  assert_eq!(m2id,
+             Int2_2::new(
+               one, zero,
+               zero, one
+             ));
+
+  let m2id = Float2_2::identity();
+  let zero = ScalarFloat::zero();
+  let one = ScalarFloat::one();
+
+  assert_eq!(m2id,
+             Float2_2::new(
+               one, zero,
+               zero, one
+             ));
 }
 
 #[test]
@@ -25,6 +49,17 @@ fn mat2_2_transpose() {
              Int2_2::from((
                Int2::from((1, 3)),
                Int2::from((2, 4))
+             )));
+
+
+  let col0 = Float2::from((1.25, 2.5));
+  let col1 = Float2::from((3.75, 5.0));
+  let m2f1 = Float2_2::from((col0, col1));
+
+  assert_eq!(m2f1.transpose().as_matrix(),
+             Float2_2::from((
+               Float2::from((1.25, 3.75)),
+               Float2::from((2.5,  5.0))
              )));
 }
 
@@ -42,6 +77,20 @@ fn mat2_2_add() {
     Int2::from((6, 8)),
     Int2::from((10, 12))
   )));
+
+
+  let col0 = Float2::from((1.25, 2.5));
+  let col1 = Float2::from((3.75, 5.0));
+  let m2f1 = Float2_2::from((col0, col1));
+
+  let col0 = Float2::from((6.25, 7.5));
+  let col1 = Float2::from((8.75, 10.0));
+  let m2f2 = Float2_2::from((col0, col1));
+
+  assert_eq!(m2f1 + m2f2, Float2_2::from((
+    Float2::from((7.5,  10.0)),
+    Float2::from((12.5, 15.0))
+  )));
 }
 
 #[test]
@@ -58,6 +107,20 @@ fn mat2_2_sub() {
     Int2::from((-4, -4)),
     Int2::from((-4, -4))
   )));
+
+
+  let col0 = Float2::from((1.25, 2.5));
+  let col1 = Float2::from((3.75, 5.0));
+  let m2f1 = Float2_2::from((col0, col1));
+
+  let col0 = Float2::from((6.25, 7.5));
+  let col1 = Float2::from((8.75, 10.0));
+  let m2f2 = Float2_2::from((col0, col1));
+
+  assert_eq!(m2f1 - m2f2, Float2_2::from((
+    Float2::from((-5.0, -5.0)),
+    Float2::from((-5.0, -5.0))
+  )));
 }
 
 #[test]
@@ -70,6 +133,16 @@ fn mat2_2_mul() {
     Int2::from((2, 4)),
     Int2::from((6, 8))
   )));
+
+
+  let col0 = Float2::from((1.25, 2.5));
+  let col1 = Float2::from((3.75, 5.0));
+  let m2f1 = Float2_2::from((col0, col1));
+
+  assert_eq!(m2f1 * ScalarFloat::new(2.0), Float2_2::from((
+    Float2::from((2.5, 5.0)),
+    Float2::from((7.5, 10.0))
+  )));
 }
 
 #[test]
@@ -81,6 +154,16 @@ fn mat2_2_div() {
   assert_eq!(m2i1 / ScalarInt::new(2), Int2_2::from((
     Int2::from((0, 1)),
     Int2::from((1, 2))
+  )));
+
+
+  let col0 = Float2::from((1.25, 2.5));
+  let col1 = Float2::from((3.75, 5.0));
+  let m2f1 = Float2_2::from((col0, col1));
+
+  assert_eq!(m2f1 / ScalarFloat::new(2.0), Float2_2::from((
+    Float2::from((0.625, 1.25)),
+    Float2::from((1.875, 2.5))
   )));
 }
 
@@ -98,6 +181,20 @@ fn mat2_2_dot() {
     Int2::from((23, 34)),
     Int2::from((31, 46))
   )));
+
+
+  let col0 = Float2::from((1.25, 2.5));
+  let col1 = Float2::from((3.75, 5.0));
+  let m2f1 = Float2_2::from((col0, col1));
+
+  let col0 = Float2::from((6.25, 7.5));
+  let col1 = Float2::from((8.75, 10.0));
+  let m2f2 = Float2_2::from((col0, col1));
+
+  assert_eq!(m2f1.dot(&m2f2), Float2_2::from((
+    Float2::from((35.9375, 53.125)),
+    Float2::from((48.4375, 71.875))
+  )));
 }
 
 #[test]
@@ -109,6 +206,15 @@ fn mat2_2_dot_vector() {
   let v2i1 = Int2::from((5, 6));
 
   assert_eq!(m2i1.dot_vector(&v2i1), Int2::from((23, 34)));
+
+
+  let col0 = Float2::from((1.25, 2.5));
+  let col1 = Float2::from((3.75, 5.0));
+  let m2f1 = Float2_2::from((col0, col1));
+
+  let v2f1 = Float2::from((6.25, 7.5));
+
+  assert_eq!(m2f1.dot_vector(&v2f1), Float2::from((35.9375, 53.125)));
 }
 
 #[test]
@@ -118,4 +224,11 @@ fn mat2_2_det() {
   let m2i1 = Int2_2::from((col0, col1));
 
   assert_eq!(m2i1.det(), ScalarInt::new(-2));
+
+
+  let col0 = Float2::from((1.25, 2.5));
+  let col1 = Float2::from((3.75, 5.0));
+  let m2f1 = Float2_2::from((col0, col1));
+
+  assert_eq!(m2f1.det(), ScalarFloat::new(-3.125));
 }
